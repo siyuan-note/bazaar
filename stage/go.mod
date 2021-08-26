6e91db433942818be608be96ad3e1978eae01745
Liang Ding <d@b3log.org>
2021-08-27 00:19:27 +0800

    :octocat: 通过 GitHub Actions 自动构建生产环境索引 https://github.com/siyuan-note/bazaar/issues/154

--- stage/go.mod
+++ stage/go.mod
@@ -4,11 +4,18 @@ go 1.17
 
 require (
 	github.com/88250/gulu v1.1.73
+	github.com/parnurzeal/gorequest v0.2.16
 	golang.org/x/text v0.3.7 // indirect
 )
 
 require (
+	github.com/elazarl/goproxy v0.0.0-20210801061803-8e322dfb79c4 // indirect
 	github.com/json-iterator/go v1.1.11 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/panjf2000/ants/v2 v2.4.6 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/smartystreets/goconvey v1.6.4 // indirect
+	golang.org/x/net v0.0.0-20210825183410-e898025ed96a // indirect
+	moul.io/http2curl v1.0.0 // indirect
 )
--- stage/main.go
+++ stage/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"crypto/tls"
 	"os"
+	"strings"
+	"sync"
+	"time"
 
 	"github.com/88250/gulu"
+	ants "github.com/panjf2000/ants/v2"
+	"github.com/parnurzeal/gorequest"
 )
 
 var logger = gulu.Log.NewLogger(os.Stdout)
@@ -11,4 +17,88 @@ var logger = gulu.Log.NewLogger(os.Stdout)
 func main() {
 	logger.Infof("bazaar is staging...")
 
+	data, err := os.ReadFile("themes.json")
+	if nil != err {
+		logger.Fatalf("read themes.json failed: %s", err)
+	}
+
+	themes := map[string]interface{}{}
+	if err = gulu.JSON.UnmarshalJSON(data, &themes); nil != err {
+		logger.Fatalf("unmarshal themes.json failed: %s", err)
+	}
+
+	repos := themes["repos"].([]interface{})
+	var stageRepos []interface{}
+	waitGroup := &sync.WaitGroup{}
+
+	p, _ := ants.NewPoolWithFunc(8, func(arg interface{}) {
+		defer waitGroup.Done()
+		repo := arg.(string)
+		t := repoUpdateTime(repo)
+		stageRepos = append(stageRepos, &stageRepo{
+			URL:     repo,
+			Updated: t,
+		})
+	})
+	for _, repo := range repos {
+		waitGroup.Add(1)
+		p.Invoke(repo)
+	}
+	waitGroup.Wait()
+	p.Release()
+
+	stageThemes := map[string]interface{}{
+		"repos": stageRepos,
+	}
+
+	data, err = gulu.JSON.MarshalIndentJSON(stageThemes, "", "  ")
+	if nil != err {
+		logger.Fatalf("marshal stage themes.json failed: %s", err)
+	}
+
+	if err = os.WriteFile("stage/themes.json", data, 0644); nil != err {
+		logger.Fatalf("write stage themes.json failed: %s", err)
+	}
+
+	logger.Infof("bazaar staged")
+}
+
+func repoUpdateTime(repoURL string) string {
+	result := filetree{}
+	request := gorequest.New().TLSClientConfig(&tls.Config{InsecureSkipVerify: true})
+	resp, _, errs := request.Get("https://data.jsdelivr.com/v1/package/gh/"+repoURL).
+		Set("User-Agent", "bazaar/1.0.0 https://github.com/siyuan-note/bazaar").Timeout(7*time.Second).
+		Retry(1, time.Second).EndStruct(&result)
+	if nil != errs {
+		//util.LogErrorf("get repo file tree failed: %s", errs)
+		return ""
+	}
+	if 200 != resp.StatusCode {
+		logger.Errorf("get repo file tree failed: %s", errs)
+		return ""
+	}
+
+	for _, f := range result.Files {
+		if strings.HasSuffix(f.Name, ".json") {
+			return f.Time
+		}
+	}
+	return ""
+}
+
+type stageRepo struct {
+	URL     string `json:"url"`
+	Updated string `json:"updated"`
+}
+
+type filetree struct {
+	Files []*file `json:"files"`
+}
+
+type file struct {
+	Type  string  `json:"type"`
+	Name  string  `json:"name"`
+	Time  string  `json:"time"`
+	Data  []byte  `json:"data"`
+	Files []*file `json:"files"`
 }
