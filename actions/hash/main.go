a5466b42e323415a565396b26ed770350a2ca895
Liang Ding <d@b3log.org>
2023-05-05 12:40:46 +0800

    :art: 集市包使用 dist.zip 进行发布 https://github.com/siyuan-note/bazaar/issues/1102

--- actions/hash/main.go
+++ actions/hash/main.go
@@ -8,23 +8,20 @@
 // THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 // See the Mulan PSL v2 for more details.
 
-package main
+package hash
 
 import (
 	"os"
 	"os/exec"
 	"strings"
-	"time"
 
 	"github.com/88250/gulu"
-	"github.com/parnurzeal/gorequest"
-	"github.com/siyuan-note/bazaar/actions/util"
 )
 
 var logger = gulu.Log.NewLogger(os.Stdout)
 
 func main() {
-	logger.Infof("bazaar is indexing...")
+	logger.Infof("bazaar is hashing...")
 
 	cmd := exec.Command("git", "rev-parse", "HEAD")
 	data, err := cmd.CombinedOutput()
@@ -34,22 +31,11 @@ func main() {
 	hash := strings.TrimSpace(string(data))
 	logger.Infof("bazaar [%s]", hash)
 
-	u := "https://rhythm.b3log.org/api/siyuan/bazaar/hash"
-	resp, data, errs := gorequest.New().Post(u).
-		SendMap(map[string]interface{}{
-			"token": os.Getenv("RHYTHEM_TOKEN"),
-			"hash":  hash,
-		}).
-		Set("User-Agent", util.UserAgent).
-		Retry(3, 3*time.Second).Timeout(30 * time.Second).EndBytes()
-	if nil != errs {
-		logger.Fatalf("hash [%s] failed: %s", u, errs)
-		return
-	}
-	if 200 != resp.StatusCode {
-		logger.Fatalf("hash [%s] failed: %d", u, resp.StatusCode)
-		return
-	}
+	stageIndex(hash, "themes")
+	stageIndex(hash, "templates")
+	stageIndex(hash, "icons")
+	stageIndex(hash, "widgets")
+	stageIndex(hash, "plugins")
 
-	logger.Infof("Hashed bazaar")
+	logger.Infof("indexed bazaar")
 }
--- actions/util/oss.go
+++ actions/util/oss.go
@@ -1,7 +1,7 @@
 // SiYuan community bazaar.
 // Copyright (c) 2021-present, b3log.org
 //
-// Pipe is licensed under Mulan PSL v2.
+// Bazaar is licensed under Mulan PSL v2.
 // You can use this software according to the terms and conditions of the Mulan PSL v2.
 // You may obtain a copy of Mulan PSL v2 at:
 //         http://license.coscl.org.cn/MulanPSL2
