7a64d6ad447aa82b69dc40247bcf63db9c331fcc
Jeffrey Chen <78434827+TCOTC@users.noreply.github.com>
2026-03-02 19:58:01 +0800

    :recycle: Merge Index bazaar with Go hashing

--- actions/index/main.go
+++ actions/index/main.go
@@ -15,6 +15,7 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/88250/gulu"
@@ -24,6 +25,7 @@ import (
 
 var logger = gulu.Log.NewLogger(os.Stdout)
 
+// 出现任何错误都立即退出中断工作流，确保一切正常才上报 hash
 func main() {
 	logger.Infof("bazaar is indexing...")
 
@@ -35,15 +37,23 @@ func main() {
 	hash := strings.TrimSpace(string(data))
 	logger.Infof("bazaar [%s]", hash)
 
-	stageIndex(hash, "themes")
-	stageIndex(hash, "templates")
-	stageIndex(hash, "icons")
-	stageIndex(hash, "widgets")
-	stageIndex(hash, "plugins")
+	indexes := []string{"themes", "templates", "icons", "widgets", "plugins"}
+	var wg sync.WaitGroup
+	wg.Add(len(indexes))
+	for _, idx := range indexes {
+		go func(i string) {
+			defer wg.Done()
+			stageIndex(hash, i)
+		}(idx)
+	}
+	wg.Wait()
+
+	reportHash(hash)
 
 	logger.Infof("indexed bazaar")
 }
 
+// stageIndex 将指定类型的集市索引上传到 OSS
 func stageIndex(hash string, index string) {
 	u := "https://raw.githubusercontent.com/siyuan-note/bazaar/" + hash + "/stage/" + index + ".json"
 	resp, data, errs := gorequest.New().Get(u).
@@ -76,5 +86,26 @@ func stageIndex(hash string, index string) {
 	if nil != err {
 		logger.Fatalf("upload bazaar stage index [%s] failed: %s", key, err)
 	}
-	logger.Infof("uploaded index [%s]", key)
+	logger.Infof("uploaded index: %s", "https://oss.b3logfile.com/"+key)
+}
+
+// reportHash 将当前 bazaar 版本 hash 上报到 Rhy，供思源客户端获知当前集市版本
+func reportHash(hash string) {
+	u := "https://rhythm.b3log.org/api/siyuan/bazaar/hash"
+	resp, _, errs := gorequest.New().Post(u).
+		SendMap(map[string]interface{}{
+			"token": os.Getenv("RHYTHEM_TOKEN"),
+			"hash":  hash,
+		}).
+		Set("User-Agent", util.UserAgent).
+		Retry(3, 3*time.Second).Timeout(30 * time.Second).EndBytes()
+	if nil != errs {
+		logger.Fatalf("hash [%s] failed: %s", u, errs)
+		return
+	}
+	if 200 != resp.StatusCode {
+		logger.Fatalf("hash [%s] failed: %d", u, resp.StatusCode)
+		return
+	}
+	logger.Infof("Hashed bazaar")
 }
